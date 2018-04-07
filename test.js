1c52bdffefe3926a9134d99c041366dc3829b0ff
Kailas Krivanka <me@kailas.xyz>
2018-04-07 15:44:43 -0700

    refactor code

--- test.js
+++ test.js
@@ -1,11 +1,34 @@
 var AWS = require('aws-sdk');
 AWS.config.loadFromPath('./config.json');
-var params = {
-  LanguageCode: "en", /* required */
-  Text: 'I love you?' /* required */
-};
+
+//constants for sentiment response
+const NEGATIVE='NEGATIVE',NEUTRAL='NEUTRAL',MIXED='MIXED',POSITIVE='POSITIVE';
+
 var comprehend = new AWS.Comprehend();
-comprehend.detectSentiment(params, function(err, data) {
-  if (err) console.log(err, err.stack); // an error occurred
-  else     console.log(data);           // successful response
-});
+var params = {
+  LanguageCode: 'en'
+};
+
+//pass string of msg to be evaled
+function evalMsg(msg){
+  params.Text = msg;
+  comprehend.detectSentiment(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);           // successful response
+    switch(data.Sentiment){
+      case NEGATIVE:
+        console.log("you get a warning");
+        break;
+      case POSITIVE:
+        break;
+      case MIXED:
+        break;
+      case NEUTRAL:
+        break;
+      default:
+        break;
+    }
+  });
+}
+
+evalMsg("every thing is the worst");
